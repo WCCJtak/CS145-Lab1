5f159ccca5766049fcae442ecd2da760d5f9785c
WCCJtak <jtak9479@student.whatcom.edu>
2024-01-08 22:31:37 -0800

    added prelab

---
+++ PreLab.java
@@ -0,0 +1,38 @@
+/**
+ * CS&145
+ * 08/01/2023
+ * Lab 1
+ * 
+ * Guessing Game
+ */
+
+// the main function
+public static void main()
+// initialize values and show the intro
+// play the game until user does not want to play again
+// if score of game is the best, update the best score
+// if the user does not want to play again show reports then teardown.
+
+// the function that plays a single game
+public static int playGame()
+// generates a random number
+// guess a number
+// until it's equal to the random number
+// returns total guesses
+
+// the function that asks the user if he wants to play again
+public static boolean playAgain()
+// return true if the user wants to play again or false
+
+// shows the instruction
+public static void help()
+
+// shows the summary of the whole game
+public static void summary()
+
+// compare two scores
+// Returns true if the score2 is better than score1.
+public static boolean isBetterScore(int score1, int score2)
+
+// Returns true if given integers are equal
+public static boolean isEqual(int num1, int num2)
